6f31981e795dc93f870a6b0838017e89a9d34d81
Faiem-qy <f.quanyoung@gmail.com>
2023-12-18 20:10:09 -0500

    implemented exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#Load the first store with id = 1 and assign it to store 1
+@store1 = Store.find(1)
+
+#Load the second store and assign it to store 2
+@store2 = Store.find(2)
+
+#Update the first store (@store)
+@store1.update(name: "New Store Name, previously store 1")
+
+puts "Updated store name: #{@store1.name}"
